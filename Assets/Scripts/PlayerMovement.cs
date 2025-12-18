b7d9028fc4d891b1316a78d54a6fd6dd5464849c
Haydos1210 <haydos1210@gmail.com>
2025-12-18 16:48:42 +1100

    Added camera movement script
    Added logic which moves position of camera game object to position of 'camPos' which is a component of player. Basically, camera moves with player.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,16 +2,37 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement Settings")]
+    [SerializeField] private float walkSpeed = 5f;
+
+    [Header("References")]
+    private CharacterController characterController;
+
+    [Header("Input")]
+    private float hInput;
+    private float vInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Input input = GetComponent<Input>();
-        transform.Translate(Vector3.forward * Time.deltaTime); 
+        UpdateInput();
+        UpdateMovement();
+    }
+
+    void UpdateMovement()
+    {
+        Vector3 newPos = new Vector3(hInput, 0, vInput);
+        characterController.Move(newPos * Time.deltaTime * walkSpeed);
+    }
+
+    private void UpdateInput()
+    {
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
     }
 }
