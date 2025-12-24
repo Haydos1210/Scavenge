6ecfe89ee03eb40f588ce33fc8aa853af1032dab
Haydos1210 <haydos1210@gmail.com>
2025-12-24 18:47:21 +1100

    fix: implemented new input system
    used unitys new input system for movement and looking

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -4,32 +4,35 @@ using UnityEngine.InputSystem;
 public class InputHandler : MonoBehaviour
 {
     [SerializeField] private PlayerController playerController;
-    private InputAction move, look, jump, sprint;
+    private PlayerControls playerControls;
+    private InputAction move, sprint, jump, look;
     void Awake()
     {
-        move = InputSystem.actions.FindAction("Move");
-        look = InputSystem.actions.FindAction("Look");
-        jump = InputSystem.actions.FindAction("Jump");
-        sprint = InputSystem.actions.FindAction("Sprint");
+        playerControls = new PlayerControls();
+        //move = InputSystem.actions.FindAction("Move");
+        //look = InputSystem.actions.FindAction("Look");
+        //jump = InputSystem.actions.FindAction("Jump");
+        //sprint = InputSystem.actions.FindAction("Sprint");
     }
 
     void OnEnable()
     {
+        move = playerControls.Player.Move;
         move.Enable();
-        look.Enable();
-        jump.Enable();
-        sprint.Enable();
 
+        look = playerControls.Player.Look;
+        look.Enable();
+
+        jump = playerControls.Player.Jump;
+        jump.Enable();
         jump.performed += OnJumpPerformed;
     }
 
     void OnDisable()
     {
-        jump.performed -= OnJumpPerformed;
         move.Disable();
         look.Disable();
         jump.Disable();
-        sprint.Disable();
     }
 
     // Update is called once per frame
@@ -41,16 +44,16 @@ public class InputHandler : MonoBehaviour
         Vector2 lookVector = look.ReadValue<Vector2>();
         playerController.RotatePlayer(lookVector);
 
-        bool isSprinting = sprint.ReadValue<float>() > 0.5f;
-        if (isSprinting)
-        {
-            Debug.Log(sprint.activeControl);
-            Debug.Log("Yes");
-        } else
-        {
-            Debug.Log("No");
-        }
-        playerController.Sprint(isSprinting);
+        //bool isSprinting = sprint.ReadValue<float>() > 0.5f;
+        //if (isSprinting)
+        //{
+        //    Debug.Log(sprint.activeControl);
+        //    Debug.Log("Yes");
+        //} else
+        //{
+        //    Debug.Log("No");
+        //}
+        //playerController.Sprint(isSprinting);
     }
 
     void OnJumpPerformed(InputAction.CallbackContext context)
